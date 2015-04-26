0627fffecf03e7309be00994ef56f45bdc4b224c
Andrea Bigagli <abigagli@gmail.com>
2015-04-26 17:33:13 +0200

    Project Kick-Off, added imageview, toolbar with camera/Album buttons and logic to selectively enable camerea

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MemeEditorViewController: UIViewController {
 
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            cameraButton.enabled = true
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
