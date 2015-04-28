2ebf1d1b2f0fd9cb2f1f828b6aed1ccc1c74ea49
Andrea Bigagli <abigagli@gmail.com>
2015-04-28 23:34:16 +0200

    Added all controllers for second scene (saved memes)

---
+++ MemeMe/SentMemeCollectionViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SentMemeCollectionViewController.swift
+//  MemeMe
+//
+//  Created by Andrea Bigagli on 28/4/15.
+//  Copyright (c) 2015 Andrea Bigagli. All rights reserved.
+//
+
+import UIKit
+
+class SentMemeCollectionViewController: UIViewController {
+    
+    var savedMemes: [Meme]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let object = UIApplication.sharedApplication().delegate
+        let appDelegate = object as! AppDelegate
+        savedMemes = appDelegate.savedMemes
+        
+        if savedMemes.count == 0 {
+            
+            let memeEditorNav = storyboard?.instantiateViewControllerWithIdentifier("MemeEditorNav") as! UINavigationController
+            presentViewController(memeEditorNav, animated: false, completion: nil)
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
