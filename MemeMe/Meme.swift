80cdd42f78c15d99327dac13069928fe3c51d9b1
Andrea Bigagli <abigagli@gmail.com>
2015-06-28 12:46:31 +0200

    Transformed the Meme model into a class with an initializer as suggested in code-review

--- MemeMe/Meme.swift
+++ MemeMe/Meme.swift
@@ -9,10 +9,17 @@
 import Foundation
 import UIKit
 
-struct Meme {
+class Meme {
     var topText: String
     var bottomText: String
     
     var originalImage : UIImage
     var memedImage : UIImage
+    
+    init (topText: String, bottomText: String, originalImage: UIImage, memedImage: UIImage) {
+        self.topText = topText
+        self.bottomText = bottomText
+        self.originalImage = originalImage
+        self.memedImage = memedImage
+    }
 }
